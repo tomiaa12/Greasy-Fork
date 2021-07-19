ec1e00da4776fdfd87fa855f3376f4e517de62c3
tomiaa12 <1178852449@qq.com>
2021-07-19 20:46:27 +0800

    更新

--- baidu.js
+++ baidu.js
@@ -16,8 +16,7 @@
 
 // @icon         https://www.baidu.com/cache/icon/favicon.ico
 // @grant        none
-
-// // @require    File://F:\WEB\demo.js
+'@require    File://F:\WEB\Greasy-Fork\baidu.js'
 // ==/UserScript==
 ;(() => {
   console.clear();
